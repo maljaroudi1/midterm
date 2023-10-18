8edd0ec3532e86e699ab50696f0763ce7e96a6dd
maljaroudi1 <meojax.ajx@gmail.com>
2023-10-18 12:36:32 -0400

    new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css'
 import axios from 'axios';
 function App() {
@@ -16,6 +17,21 @@ function App() {
 
   async function  onSubmitProduct() {
     try{
+      if(product === ''){
+        toast.error('Please enter a product')
+      }
+      if(name === ''){
+        toast.error('Please enter a name')
+      }
+      if(price === ''){
+        toast.error('Please enter a price')
+      }
+      if(quantity === ''){
+        toast.error('Please enter a quantity')
+      }
+      if(category === ''){
+        toast.error('Please select a category')
+      }
       const response = await axios.post('http://localhost:5000/api/product',{
         product,
         name,
@@ -23,16 +39,23 @@ function App() {
         quantity,
         category
       });
-
+      toast.success('Product was subbmited!')
       console.log(response)
     }catch(error){
+      toast.error('Error')
       console.log(error)
     }
 
   }
 
+  function onCancel() {
+    window.location.href="/"
+  }
+
   return (
     <>
+    <ToastContainer/>
+    <h2>New product</h2>
       <div className="form-container">
         <div className="form-products" style={{display: 'grid'}}>
           <label htmlFor="">Product</label>
@@ -42,10 +65,10 @@ function App() {
           <input type="text" onChange={(e) => setName(e.target.value)} />
 
           <label htmlFor="">Price</label>
-          <input type="number" onChange={(e) => setPrice(e.target.value)} />
+          <input type="text" onChange={(e) => setPrice(e.target.value)} />
 
           <label htmlFor="">Quantity</label>
-          <input type="number" onChange={(e) => setQuantity(e.target.value)} />
+          <input type="text" onChange={(e) => setQuantity(e.target.value)} />
 
           <label htmlFor="">Category</label>
           <select name="" id="" onChange={(e) => setCategory(e.target.value)}>Category
@@ -54,12 +77,10 @@ function App() {
               <option value="Women">Women</option>
               <option value="Teens">Teens</option>
           </select>
-          <button onClick={onSubmitProduct}>Submit</button>
-
+          <button onClick={onSubmitProduct} style={{transform: 'translateY(1rem)'}}>Submit</button>
+          <button onClick={onCancel} style={{transform: 'translateY(2rem)'}}>Cancel</button>
         </div>
-        <div className="form-catagories">
 
-        </div>
       </div>
     </>
   );
